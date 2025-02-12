0e579a9891e015693c029af508e4c788e1b15f38
judahtc <jaycea247@gmail.com>
2025-02-12 22:07:10 +0200

    feat: adjusted the whats app logo on my portfolio  for small screens

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
                     <Outlet />
                 </div>
 
-                <div className="whatsapp fixed right-2 bottom-40">
+                <div className="whatsapp fixed right-3 lg:right-10 bottom-5 lg:bottom-10">
                     <Link to="https://wa.me/+263785430774">
                         <svg
                             to="https://wa.me/+@63785430774"
