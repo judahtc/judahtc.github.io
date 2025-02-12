246c633b00617ad93023eb5dc45ab7b8f5f229af
judahtc <jaycea247@gmail.com>
2025-02-12 11:19:36 +0200

    feat: added databases in the explored skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,9 @@
 export default function Skills() {
+    type itemType = {
+        name: String;
+        technologies: [String];
+        icon: any;
+    };
     const item: any = [
         {
             name: "Frontend",
@@ -80,10 +85,31 @@ export default function Skills() {
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    className="lucide lucide-code"
+                    className="lucide lucide-cloud"
                 >
-                    <polyline points="16 18 22 12 16 6" />
-                    <polyline points="8 6 2 12 8 18" />
+                    <path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
+                </svg>
+            ),
+        },
+        {
+            name: "Databases",
+            technologies: ["Postgres", "MySQL", "SSMS", "Dynamo", "Oracle"],
+            icon: (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="lucide lucide-database"
+                >
+                    <ellipse cx="12" cy="5" rx="9" ry="3" />
+                    <path d="M3 5V19A9 3 0 0 0 21 19V5" />
+                    <path d="M3 12A9 3 0 0 0 21 12" />
                 </svg>
             ),
         },
@@ -96,24 +122,6 @@ export default function Skills() {
 }
 
 export function Card({ item }) {
-    const frontend_icon = (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="34"
-            height="34"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-laptop-minimal"
-        >
-            <rect width="18" height="12" x="3" y="4" rx="2" ry="2" />
-            <line x1="2" x2="22" y1="20" y2="20" />
-        </svg>
-    );
-
     return (
         <>
             <div className="grid grid-cols-2 mx-32">
