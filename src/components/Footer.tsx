574a6a1f5203bd877091f6b3f2f0a52c72c95367
judahtc <jaycea247@gmail.com>
2025-02-17 11:45:16 +0200

    refactor: dealth with the footer for

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
     return (
         <>
             <div
-                className="container mx-auto w-4/5 lg:w-1/2 md:w-1/2 p-4 mt-32 mb-32"
+                className="container mx-auto w-4/5 lg:w-1/2 md:w-1/2 p-4 mt-32 mb-32 dark:text-zinc-400"
                 id="footer"
             >
                 <div className="flex flex-col items-center">
@@ -44,7 +44,7 @@ export default function Footer() {
                     </div>
                     <div>
                         <div className=" flex items-center space-x-2  mt-6 font-semibold lg:text-base md:text-base text-sm">
-                            <div className="linkedin dark:bg-transparent dark:text-white dark:border dark:border-gray-50 bg-gray-100 hover:bg-gray-200 px-2 py-[0.4rem] pr-3  rounded">
+                            <div className="linkedin dark:bg-transparent dark:text-zinc-400 dark:border dark:border-zinc-900 bg-gray-100 hover:bg-gray-200 px-2 py-[0.4rem] pr-3  rounded">
                                 <a
                                     target="_blank"
                                     href="https://www.linkedin.com/in/judah-t-chisare-a1b998174/"
@@ -75,7 +75,7 @@ export default function Footer() {
                                     <span>LinkedIn</span>
                                 </a>
                             </div>
-                            <div className="email dark:bg-transparent dark:text-white dark:border dark:border-gray-50 bg-gray-100 hover:bg-gray-200 px-2 py-[0.4rem] pr-3  rounded">
+                            <div className="email dark:bg-transparent dark:text-zinc-400 dark:border dark:border-zinc-900 bg-gray-100 hover:bg-gray-200 px-2 py-[0.4rem] pr-3  rounded">
                                 {" "}
                                 <a
                                     target="_blank"
@@ -107,7 +107,7 @@ export default function Footer() {
                                 </a>
                             </div>
 
-                            <div className="github bg-gray-100 dark:bg-transparent dark:text-white dark:border dark:border-gray-50 hover:bg-gray-200 px-2 py-[0.4rem] pr-3  rounded">
+                            <div className="github bg-gray-100 dark:bg-transparent dark:text-zinc-400 dark:border dark:border-zinc-900 hover:bg-gray-200 px-2 py-[0.4rem] pr-3  rounded">
                                 <a
                                     target="_blank"
                                     href="https://drive.google.com/file/d/1bvldXKYswjGPBBtjz6uMf2cPtlrX97fl/view?usp=drive_link"
