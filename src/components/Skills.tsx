9b2181656842b46139f7a7dec301fbb945d92af4
judahtc <jaycea247@gmail.com>
2025-03-06 08:13:31 +0200

    fix: made the resume button under skills smaller

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -274,7 +274,10 @@ export function Card({ item }: any) {
                 <div className="lg:mx-40 mx-7">
                     <Dialog>
                         <DialogTrigger>
-                            <Button className="mr-3 bg-transparent text-zinc-900 dark:text-zinc-400 border dark:hover:bg-zinc-900 hover:bg-gray-50 border-gray-200">
+                            <Button
+                                size={"sm"}
+                                className="mr-3 bg-transparent text-zinc-900 dark:text-zinc-400 border dark:hover:bg-zinc-900 hover:bg-gray-50 border-gray-200"
+                            >
                                 Resume <Download />
                             </Button>
                         </DialogTrigger>
