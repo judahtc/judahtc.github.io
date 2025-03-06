35c7520932ef943b8862839f88282bcb467199dc
judahtc <jaycea247@gmail.com>
2025-03-06 10:35:43 +0200

    feat:created and filled the column component

---
+++ src/components/hobbies/columns.tsx
@@ -0,0 +1,17 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { Payment } from "./lodder";
+
+export const columns: ColumnDef<Payment>[] = [
+    {
+        accessorKey: "status",
+        header: "Status",
+    },
+    {
+        accessorKey: "email",
+        header: "Email",
+    },
+    {
+        accessorKey: "amount",
+        header: "Amount",
+    },
+];
