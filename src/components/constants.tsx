d3bdce2568f66dce7c36e7bcd5032ac918411147
judahtc <jaycea247@gmail.com>
2025-03-07 16:21:58 +0200

    feat: added my linkedin link constant

--- src/components/constants.tsx
+++ src/components/constants.tsx
@@ -2,3 +2,5 @@ export const name = "Judah";
 export const email = "jaycea247@gmail.com";
 export const phone = "+263785430774";
 export const github = "https://github.com/judahtc";
+export const linkedin =
+    "https://www.linkedin.com/in/judah-t-chisare-a1b998174/";
